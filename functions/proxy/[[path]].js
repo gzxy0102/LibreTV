6b1a7c8aba3723b0beff5d7a9f1174dacea3ec03
听风 <gaizhixin@vip.qq.com>
2025-08-29 10:22:13 +0800

    Update [[path]].js

--- functions/proxy/[[path]].js
+++ functions/proxy/[[path]].js
@@ -24,7 +24,7 @@ const MEDIA_CONTENT_TYPES = ['video/', 'audio/', 'image/'];
  * 主要的 Pages Function 处理函数
  * 拦截发往 /proxy/* 的请求
  */
-export async function onRequest(context) {
+export function onRequest(context) {
     const { request, env, next, waitUntil } = context; // next 和 waitUntil 可能需要
     console.log("环境变量",env)
     const url = new URL(request.url);
